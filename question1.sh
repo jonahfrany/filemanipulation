272df97cd90cb11e37b66c1952170f7fb8301b09
jonahfrany <39085016+jonahfrany@users.noreply.github.com>
2018-05-08 18:10:21 +0800

    Create question1.sh

---
+++ question1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Write a program in /app that will write 2 MB worth of file (contents can be gibberish) to /data. 
+# Ensure that this will be ran every 10 minutes.
+
+dd if=/dev/urandom of=output_file.txt count=1024 bs=2024

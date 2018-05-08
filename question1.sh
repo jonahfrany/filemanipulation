6be3a2f9f6081341d7ceb7e9e760c4bffd850283
jonahfrany <39085016+jonahfrany@users.noreply.github.com>
2018-05-08 18:14:04 +0800

    Update question1.sh

--- question1.sh
+++ question1.sh
@@ -2,4 +2,5 @@
 # Write a program in /app that will write 2 MB worth of file (contents can be gibberish) to /data. 
 # Ensure that this will be ran every 10 minutes.
 
-dd if=/dev/urandom of=/app/output_file.txt count=1024 bs=2024
+cp question1.sh /app
+dd if=/dev/urandom of=/data/output_file.txt count=1024 bs=2024

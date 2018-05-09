ee4aa7dc448f3b6e00c704381f917ec763b54480
jonahfrany <39085016+jonahfrany@users.noreply.github.com>
2018-05-09 14:28:56 +0800

    Update question1.sh

--- question1.sh
+++ question1.sh
@@ -2,5 +2,9 @@
 # Write a program in /app that will write 2 MB worth of file (contents can be gibberish) to /data. 
 # Ensure that this will be ran every 10 minutes.
 
-cp question1.sh /app
-dd if=/dev/urandom of=/data/output_file.txt count=1024 bs=2024
+echo "export now=$(date +"%m_%d_%Y")" >> /app/create_file.sh
+echo "dd if=/dev/urandom of=/data/output_file_$now.txt count=1024 bs=2024" >> /app/create_file.sh
+crontab -l > user_cron
+echo "10 * * * * ./app/create_file.sh" >> user_cron
+crontab user_cron
+rm user_cron
